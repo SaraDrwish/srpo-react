36f356f884b8a317f0ec7c2dd99f798656e1f932
Sara <38770404+SaraDrwish@users.noreply.github.com>
2023-06-06 21:50:21 +0300

    f c

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+         <h2 className="App-logo" >-Sara-</h2>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
