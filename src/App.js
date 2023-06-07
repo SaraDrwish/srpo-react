f43643ea6747d58d47e91464d838b2d752f734ed
Sara <38770404+SaraDrwish@users.noreply.github.com>
2023-06-07 09:12:38 +0300

    2th

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-        
-         <h2 className="App-logo" >-Sara-</h2>
+            <h5>Welcome in</h5>
+            <div className="App-logo" >
+              <h1> SRPO </h1>
+            </div>
+            <h6>Main React App Page </h6>
 
       </header>
     </div>
