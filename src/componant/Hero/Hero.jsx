643f2f2a4d8576cf8af845e70837960a0203c09a
Sara <38770404+SaraDrwish@users.noreply.github.com>
2023-06-09 22:07:07 +0300

    10:07

--- src/componant/Hero/Hero.jsx
+++ src/componant/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useEffect , useState } from 'react';
 import "./hero.css";
 import Header from '../Header/Header';
 import heroimg from "../../assets/img/beu.jpg"
@@ -9,14 +10,33 @@ import NumberCounter from "number-counter";
 // import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 
-  
-
 const Hero = () => {
 
   const mobile = window.innerWidth <= 768 ? true : false;
 
   const transition = { type: "spring", duration: 3 }
   
+
+  const [backBtn, setBackBtn] = useState(false)
+  
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 500) {
+        setBackBtn(true)
+      } else {
+        setBackBtn(false)
+      }
+      } )
+  
+  } , [])
+  
+  const scrollUp =()=> window.scrollTo({
+    top: 0,
+    behavior:"smooth"
+  })
+// scrollBtn =
+
+
   return (
     <div className='hero'>
 
@@ -113,11 +133,16 @@ const Hero = () => {
 
       </div> {/**end right sec */}
 
-
-       <svg  className=' ' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+      {/* onClick={scrollBtn>=1200? "show" : " " }  */}
+      
+      { backBtn &&
+      
+        <svg  onClick={scrollUp} className='btnDown' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
         <path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM135.1 217.4c-4.5 4.2-7.1 10.1-7.1 16.3c0 12.3 10 22.3 22.3 22.3H208v96c0 17.7 14.3 32 32 32h32c17.7 0 32-14.3 32-32V256h57.7c12.3 0 22.3-10 22.3-22.3c0-6.2-2.6-12.1-7.1-16.3L269.8 117.5c-3.8-3.5-8.7-5.5-13.8-5.5s-10.1 2-13.8 5.5L135.1 217.4z" />
        </svg>
 
+      }
+      
 
       
 
